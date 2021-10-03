3307dbb8b681d18597165d71903f049fe63d2316
Pedro Bernardes <phec06@gmail.com>
2021-10-03 13:03:19 -0300

    fix-env

--- app/javascript/env.js
+++ app/javascript/env.js
@@ -3,7 +3,7 @@ const ENV = {};
 if(process.env.NODE_ENV == "development") {
   ENV.API_HOST = "https://fedeaux.ngrok.io"
 } else {
-  ENV.API_HOST = "https://dodo.herokuapp.com"
+  ENV.API_HOST = "https://fedeaux-dodo.herokuapp.com"
 }
 
 export default ENV;
